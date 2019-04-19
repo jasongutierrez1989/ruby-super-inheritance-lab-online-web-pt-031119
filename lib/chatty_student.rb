ecdea5c4bebf0882028eddd1dd116fda9a54d505
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-19 03:28:51 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,14 +8,21 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
+    super
     puts "Pick me!"
-    puts "Pick me!"
+    super
     puts "Pick me!"
   end
 end
